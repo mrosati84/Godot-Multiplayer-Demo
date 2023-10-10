9fc37711851117051d2461e54016dc16152f5d73
Matteo Rosati <matteo.rosati@akqa.com>
2023-10-10 14:56:11 +0200

    refactor connect only when click join

--- multiplayer.gd
+++ multiplayer.gd
@@ -1,19 +1,39 @@
 extends Node2D
 
-var peer = ENetMultiplayerPeer.new()
-@export var player : PackedScene
+const PlayerScene = preload("res://player.tscn")
+
+func _ready():
+	# Start the server if Godot is passed the "--server" argument,
+	# and start a client otherwise.
+	if "--server" in OS.get_cmdline_args():
+		start_network(true)
 
 func _on_join_pressed():
-	peer.create_client("127.0.0.1", 8910)
+	start_network(false)
+
+func start_network(server: bool):
+	var peer = ENetMultiplayerPeer.new()
+	if server:
+		multiplayer.peer_connected.connect(self.create_player)
+		multiplayer.peer_disconnected.connect(self.destroy_player)
+		
+		peer.create_server(4242)
+		print('server listening on localhost 4242')
+	else:
+		var targetIP = "localhost"
+		peer.create_client(targetIP, 4242)
+
 	multiplayer.multiplayer_peer = peer
 
-func _on_host_pressed():
-	peer.create_server(8910)
-	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(_add_player)
-	_add_player()
+func create_player(id):
+	# Instantiate a new player for this client.
+	var p = PlayerScene.instantiate()
 
-func _add_player(id = 1):
-	var player_instance = player.instantiate()
-	player_instance.name = str(id)
-	$SpawnPoint.add_child(player_instance)
+	# Set the name, so players can figure out their local authority
+	p.name = str(id)
+	
+	$SpawnPoint.add_child(p)
+
+func destroy_player(id):
+	# Delete this peer's node.
+	$SpawnPoint.get_node(str(id)).queue_free()
