7fd965a29960a79f552660dbe6bf231fc6304c14
Matteo Rosati <rosati.matteo@gmail.com>
2023-10-13 12:47:59 +0200

    add bullet

--- components/server.gd
+++ components/server.gd
@@ -1,1 +1,17 @@
 extends Node
+
+@onready var bullet = load("res://bullet.tscn")
+@onready var spawn_point = get_node("/root/Multiplayer/SpawnPoint")
+@onready var seq = 0
+
+@rpc("any_peer")
+func fire(pos, rot):
+	var sender_id = multiplayer.get_remote_sender_id()
+	var b = bullet.instantiate()
+	b.name = str(sender_id) + str(seq)
+	seq += 1
+	b.global_position = pos
+	b.global_rotation = rot
+	
+	spawn_point.add_child(b)
+	print("FIRE!")
--- player.gd
+++ player.gd
@@ -2,12 +2,19 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+var server : Node
+const SERVER : int = 1
+
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
+func _ready():
+	var root = get_tree().root
+	server = root.get_node("/root/Multiplayer/Server")
+
 func _process(_delta):
-	if Input.is_action_pressed("fire"):
-		print("Fire")
+	if Input.is_action_just_pressed("fire"):
+		server.rpc_id(SERVER, "fire", global_position, global_rotation)
 
 func _physics_process(_delta):
 	if is_multiplayer_authority():
