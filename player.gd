769b736accf47fe9f10886ece28266a7eae232b9
Matteo Rosati <matteo.rosati@akqa.com>
2023-10-13 09:44:17 +0200

    add rotation

--- player.gd
+++ player.gd
@@ -5,8 +5,13 @@ extends CharacterBody2D
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 
+func _process(_delta):
+	if Input.is_action_pressed("fire"):
+		print("Fire")
+
 func _physics_process(_delta):
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("left", "right", "up", "down") * speed
+		look_at(get_global_mouse_position())
 	
 	move_and_slide()
