61fc4bfd268351f75888a5c968ed8c237450607b
Matteo Rosati <rosati.matteo@gmail.com>
2023-10-15 13:41:55 +0200

    use rpc for despawning

--- bullet.gd
+++ bullet.gd
@@ -21,8 +21,10 @@ func _on_body_entered(body):
 	if multiplayer.is_server():
 		print("I am the server, and " + body.name + " has been hit by " + sender)
 		var target = spawn_point.get_node(str(body.name))
-		target.rpc_id(int(str(target.name)), "damage", damage)
-		call_deferred("queue_free")
+		var target_id = int(str(target.name))
+		
+		target.rpc_id(target_id, "damage", damage)
+		despawn.rpc()
 
 @rpc("any_peer", "call_local")
 func despawn():
