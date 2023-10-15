7d9dcfcf572c890e85e7699203798779a516904b
Matteo Rosati <rosati.matteo@gmail.com>
2023-10-15 12:49:44 +0200

    add restart button (not hooked up)

--- bullet.gd
+++ bullet.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var up = Vector2.RIGHT.rotated(rotation)
 @onready var spawn_point = get_node("/root/Multiplayer/SpawnPoint")
 
+# contains the name of the peer that has originated the bullet by shooting
 var sender : String
 
 func _physics_process(delta):
@@ -18,8 +19,9 @@ func _on_timer_timeout():
 
 func _on_body_entered(body):
 	if multiplayer.is_server():
-		print("Sono il server, e " + body.name + " è stato colpito da " + sender)
-		spawn_point.get_node(str(body.name)).rpc_id(int(str(body.name)), "damage", damage)
+		print("I am the server, and " + body.name + " has been hit by " + sender)
+		var target = spawn_point.get_node(str(body.name))
+		target.rpc_id(int(str(target.name)), "damage", damage)
 		call_deferred("queue_free")
 
 @rpc("any_peer", "call_local")
--- player.gd
+++ player.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 @export var speed = 400
 @export var bullet : PackedScene
 @export var life : int = 100
+@export var alive : bool = true
 
 @onready var life_label = get_node("/root/Multiplayer/UI/PanelContainer/GridContainer/Life")
 @onready var id_label = get_node("/root/Multiplayer/UI/PanelContainer/GridContainer/ID")
+@onready var restart = get_node("/root/Multiplayer/UI/PanelContainer/GridContainer/Restart")
 @onready var server = get_node("/root/Multiplayer/Server")
 
 const SERVER : int = 1
@@ -22,19 +24,25 @@ func _ready():
 	global_position = Vector2.ZERO
 
 func _process(_delta):
-	if Input.is_action_just_pressed("fire") and is_multiplayer_authority():
+	if Input.is_action_just_pressed("fire") and is_multiplayer_authority() and alive:
 		server.rpc_id(SERVER, "fire")
 
 func _physics_process(_delta):
-	if is_multiplayer_authority():
+	if is_multiplayer_authority() and alive:
 		velocity = Input.get_vector("left", "right", "up", "down") * speed
 		look_at(get_global_mouse_position())
 
 		move_and_slide()
 
-@rpc("any_peer")
+@rpc("any_peer", "call_local")
 func die():
-	call_deferred("queue_free")
+	hide()
+	alive = false
+	global_position = Vector2(0, 0)
+	$CollisionShape2D.disabled = true
+	
+	if is_multiplayer_authority():
+		restart.show()
 
 @rpc("any_peer")
 func damage(value):
