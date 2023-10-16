8cbdd5a15e0e7ceb7ecdac10f98ab830864d023a
Fabrizio Codello <fabrizio.codello@akqa.com>
2023-10-16 20:28:12 +0200

    Added PING?PONG!

--- autoload/network_manager.gd
+++ autoload/network_manager.gd
@@ -7,8 +7,9 @@ const ADDR = "127.0.0.1"
 
 @onready var spawn_point = get_node("/root/World/SpawnPoint")
 @onready var address = get_node("/root/World/UI/PanelContainer/GridContainer/Address")
-
 var players_list = []
+var ping_label
+var ping_timer
 
 func _ready():
 	# Start the server if Godot is passed the "--server" argument,
@@ -27,8 +28,39 @@ func start_network(server: bool):
 	else:
 		var address_parts = address.text.strip_edges().split(":")
 		peer.create_client(address_parts[0], int(address_parts[1]))
-
+		
+		ping_timer = Timer.new()
+		ping_timer.set_wait_time(1.0) # 1 second delay
+		ping_timer.set_one_shot(false) # Execute cyclically
+		ping_timer.timeout.connect(_on_ping_timer_timeout)
+		add_child(ping_timer)
+		
+		ping_timer.start()
+		
+		ping_label = get_node("/root/World/UI/PanelContainer/GridContainer/Ping");
+		update_ping()
+		
 	multiplayer.multiplayer_peer = peer
+	
+func _on_ping_timer_timeout():
+	update_ping()
+	
+func update_ping():
+	var start_time = Time.get_unix_time_from_datetime_string(Time.get_time_string_from_system())
+#	print("Ping?")
+	rpc_id(1, "return_ping", start_time)
+	
+@rpc("any_peer", "call_local")
+func return_ping(received_start_time):
+	if multiplayer.is_server():
+		rpc_id(multiplayer.get_remote_sender_id(), "calculate_ping", received_start_time)
+
+@rpc("any_peer", "call_local")
+func calculate_ping(start_time):
+	var end_time = Time.get_unix_time_from_datetime_string(Time.get_time_string_from_system())
+	var ping = end_time - start_time
+	ping_label.text = "Ping: " + str(ping) + " ms"
+#	print("Pong!"+ str(ping) +"ms")
 
 func create_player(id):
 	# Instantiate a new player for this client.
